31c2389b6b50707a70199bc3ac9c07931ccd91b5
Geoff Horowitz <30778962+geoffhorowitz@users.noreply.github.com>
2023-03-11 23:33:03 -0500

    update for admobs version 21.5.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AndroidApp_AdMob_Testing
 A repo to test out Google Ad Mob
+
+Following guide from google: https://developers.google.com/admob/android/banner
--- app/src/main/java/com/example/adaptiveanchorbanner/MainActivity.java
+++ app/src/main/java/com/example/adaptiveanchorbanner/MainActivity.java
@@ -4,9 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import android.graphics.Rect;
 import android.widget.FrameLayout;
-import androidx.appcompat.app.AppCompatActivity;
 import android.view.Display;
 import android.util.DisplayMetrics;
 import com.google.android.gms.ads.AdRequest;
@@ -18,6 +16,9 @@ import com.google.android.gms.ads.initialization.OnInitializationCompleteListene
 
 public class MainActivity extends AppCompatActivity {
 
+    // This is an ad unit ID for a test ad. Replace with your own banner ad unit ID.
+    private static final String AD_UNIT_ID = "ca-app-pub-1011368772130588/6033314866";
+    //private static final String TAG = "MainActivity";
     private FrameLayout adContainerView;
     private AdView adView;
 
@@ -33,16 +34,61 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        // Set your test devices. Check your logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345"))
+        // to get test ads on this device."
+        /*MobileAds.setRequestConfiguration(
+                new RequestConfiguration.Builder().setTestDeviceIds(Arrays.asList("ABCDEF012345")).build());
+        */
+
+        // // additional note
+        // // from https://stackoverflow.com/questions/60539407/after-updating-google-ads-sdk-addtestdevice-is-deprecated-how-to-resolve
+        // // as input to setTestDeviceIds(testDevices) where defined by:
+        // // List<String> testDevices = new ArrayList<>();
+        // // testDevices.add(AdRequest.DEVICE_ID_EMULATOR);
+        // //
+
         //get the reference to your FrameLayout
         adContainerView = findViewById(R.id.adView_container);
 
-        //Create an AdView and put it into your FrameLayout
-        adView = new AdView(this);
-        adContainerView.addView(adView);
-        adView.setAdUnitId("ca-app-pub-1011368772130588/6033314866");
+        // Since we're loading the banner based on the adContainerView size, we need to wait until this
+        // view is laid out before we can get the width.
+        adContainerView.post(new Runnable() {
+            @Override
+            public void run() {
+                //start requesting banner ads
+                loadBanner();
+            }
+        });
 
-        //start requesting banner ads
-        loadBanner();
+    }
+
+    /** Called when leaving the activity */
+    @Override
+    public void onPause() {
+        if (adView != null) {
+            adView.pause();
+        }
+        super.onPause();
+    }
+
+    /** Called when returning to the activity */
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (adView != null) {
+            adView.resume();
+        }
+    }
+
+    /** Called before the activity is destroyed */
+    @Override
+    public void onDestroy() {
+        if (adView != null) {
+            adView.destroy();
+        }
+        super.onDestroy();
     }
 
     private AdSize getAdSize() {
@@ -51,20 +97,30 @@ public class MainActivity extends AppCompatActivity {
         DisplayMetrics outMetrics = new DisplayMetrics();
         display.getMetrics(outMetrics);
 
-        float widthPixels = outMetrics.widthPixels;
         float density = outMetrics.density;
 
+        float adWidthPixels = adContainerView.getWidth();
+
+        // If the ad hasn't been laid out, default to the full screen width.
+        if (adWidthPixels == 0) {
+            adWidthPixels = outMetrics.widthPixels;
+        }
+
         //you can also pass your selected width here in dp
-        int adWidth = (int) (widthPixels / density);
+        int adWidth = (int) (adWidthPixels / density);
 
         //return the optimal size depends on your orientation (landscape or portrait)
         return AdSize.getCurrentOrientationAnchoredAdaptiveBannerAdSize(this, adWidth);
     }
 
     private void loadBanner() {
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
-                .build();
+        //Create an AdView and put it into your FrameLayout
+        adView = new AdView(this);
+        adView.setAdUnitId(AD_UNIT_ID);
+        adContainerView.removeAllViews();
+        adContainerView.addView(adView);
+
+        AdRequest adRequest = new AdRequest.Builder().build();
 
         AdSize adSize = getAdSize();
         // Set the adaptive ad size to the ad view.
